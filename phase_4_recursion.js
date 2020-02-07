5d6ef75e6f477d95a295710638dfd3907b327b9f
camfilho <camfilho@ufrn.edu.br>
2020-02-06 21:39:17 -0300

    add range and range2 functions

---
+++ phase_4_recursion.js
@@ -0,0 +1,18 @@
+console.log('Phase 4 - Recursion')
+
+function range(start, end, result = []) {
+    result.push(start);
+    if(start == end) return result;
+
+    return range(start + 1, end, result);
+}
+
+function range2(start, end) {
+    if(start == end) return [];
+
+    let result = range(start, end - 1);
+    result.push(end);
+    return result;
+}
+
+console.log(range2(3,5));
