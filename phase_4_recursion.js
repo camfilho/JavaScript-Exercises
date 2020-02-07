7ed143e53bedf5916819feeadb7458089efb6d0c
camfilho <camfilho@ufrn.edu.br>
2020-02-07 10:00:43 -0300

    add recursive fibonacci with memoization

--- phase_4_recursion.js
+++ phase_4_recursion.js
@@ -25,3 +25,24 @@ function sumRec(arr){
 }
 
 console.log(sumRec([1,2,3,4]));
+
+function exponent(base, exp){
+    if (exp == 0) return 1;
+
+    return base*exponent(base,  exp - 1);
+}
+
+console.log(` 2 to the power of 3 = ${exponent(2,3)}`);
+function sleep() {
+    for(let i = 0; i < 1000*1000*1000; i++ ){
+
+    }
+}
+function fibonacci(n){
+    if (n < 2) return [1, 1];
+
+    let cache = fibonacci(n - 1);
+    cache.push(cache.slice(-2).reduce((acc, e) => acc + e));
+
+    return cache;
+}
